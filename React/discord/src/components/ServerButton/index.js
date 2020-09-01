93bba989a51ad8c807ced48f8eab5725cad4dc42
derek-alves <derek.alves@fatec.sp.gov.br>
2020-08-31 23:51:00 -0300

    side bar notification and mentions

--- React/discord/src/components/ServerButton/index.js
+++ React/discord/src/components/ServerButton/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
+import Logo from '../../assets/Logo.svg';
 import { Button } from './styles';
 
-function ServerButton() {
-  return <div />;
+function ServerButton({
+  selected = null,
+  isHome = null,
+  hasNotifications = null,
+  mentions = null,
+
+}) {
+  return (
+    <Button
+    isHome={isHome}
+    hasNotifications={hasNotifications}
+    mentions={mentions}
+    className={selected ? 'active': ''}
+
+    >
+      {isHome && <img src={Logo} />}
+    </Button>
+  )
 }
 
 export default ServerButton;
--- React/discord/src/components/ServerList/index.js
+++ React/discord/src/components/ServerList/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import ServerButton from '../ServerButton';
 import { Container,Separator } from './styles';
 
 function ServerList() {
