2ffc614ec50107eeacb8d57878ed8707332973e0
derek-alves <derek.alves@fatec.sp.gov.br>
2020-09-02 15:33:33 -0300

    Server name/Channel info / Channel list

---
+++ React/discord/src/components/ServerName/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Container,Title,ExpandIcon} from './styles';
+
+
+const ServerName = () => {
+  return (
+     <Container>
+          <Title>Servidor do Derek</Title>
+
+          <ExpandIcon/>
+     </Container>
+  );
+} 
+
+export default ServerName;
